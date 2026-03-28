b85c9a57632660f05578bc8fbbc51a1e4715613a
Chris Vam <chrisanthosva@gmail.com>
2026-03-28 17:26:41 +0200

    Fix: Complete removal of Jekyll artifacts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: '/orange_-tomato/', // VERY IMPORTANT for GitHub Pages
+  base: '/orange_-tomato/',
   plugins: [
     react(),
     tailwindcss(),
